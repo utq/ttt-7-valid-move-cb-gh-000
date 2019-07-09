135fdd325f90b6017d1b033e4d8cd438c0360f87
Celal Cankaya <celalcankaya@outlook.com>
2019-07-09 16:23:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
 # code your #valid_move? method here
+def valid_move?(board, pos)
+  if pos > board.length -1
+    return false
+  else
+    return position_taken(board, pos)
+  end
 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(baord, pos)
+  posItem = board[pos]
+  if posItem == " " or posItem == "" or posItem == nil
+    return false
+  else
+    return true
+end
